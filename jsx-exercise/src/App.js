7cd410ac7f7725d12ab3b7f943221bc7ef289f67
sseazn <sebsilva10@hotmail.com>
2025-03-07 21:23:20 -0700

    Exercise 2 implementation

--- jsx-exercise/src/App.js
+++ jsx-exercise/src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Home from './Home';
+import About from './About';
+import Contact from './Contact';
 
 function App() {
+  // Exercise 1: Embed JavaScript Expressions in JSX || Using a conditional statement in JSX
+  const currentYear = new Date().getFullYear();
+  const isLoggedIn = true;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {/* Exercise 1 */}
+
+      <h1>ENSF-381: Full Stack Web Development</h1>
+      <p>React Components</p>
+      <p>Current Year: {currentYear}</p>
+      <p>{isLoggedIn ? "Welcome back!" : "Please log in."}</p>
+      
+      {/* Exercise 2 */}
+
+      <Home title = "Home Page" description = "Welcome to our website" />
+      <About title = "About Us" description = "We are passionate about delivering quality experiences" />
+      <Contact title = "Contact Us" description = "Feel free to reach out to us via email or phone." />
+
+      {/* Exercise 3: Lists and Modularization */}
+
+      
+
     </div>
   );
 }
